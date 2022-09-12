555f7fe0a389ed15cc1b4d70a177d37fe4bd880a
Bianca Henderson <bhenderson@anaconda.com>
2022-09-12 13:40:45 -0400

    Update ascii graph module to raise exceptions properly, change summary

--- ascii_graph.py
+++ ascii_graph.py
@@ -7,12 +7,17 @@ import conda.plugins
 def ascii_graph(coordinates: str):
     try:
         to_graph = [float(x) for x in coordinates[0].split(',')]
-    except ValueError:
-        raise SystemExit("You can only graph numbers!")
+    except:
+        raise SystemExit('You can only graph numbers!\n'
+              '(the numbers must be contained in a comma-separated'
+              ' string, e.g., "-4, 5, 6.37")'
+        )
 
     if len(to_graph) != 3:
-        print("Please input a string of three numbers to graph.")
-        raise SystemExit
+        raise SystemExit('Please input a string of exactly three numbers to graph!\n'
+              '(the numbers must be comma-separated and wrapped'
+              ' with quotation marks, e.g., "-4, 5, 6.37")'
+        )
 
     s = symbols('s')
     x, y, z = [to_graph[i] for i in (0, 1, 2)]
@@ -23,6 +28,6 @@ def ascii_graph(coordinates: str):
 def conda_subcommands():
     yield conda.plugins.CondaSubcommand(
         name="ascii-graph",
-        summary="tutorial subcommand that takes in 3 ints and prints out an ascii graph",
+        summary="A subcommand that takes a string of three comma-separated numbers and prints out an ascii graph",
         action=ascii_graph,
     )
