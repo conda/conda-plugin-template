e2d3eaa73158450c3d42b1388a08d4220c59d1f8
Bianca Henderson <bhenderson@anaconda.com>
2022-09-19 12:14:16 -0400

    New Subcommand Tutorial  (#11)
    * New subcommand tutorial
    
    This new tutorial will not require anything that is not in the default Anaconda channels
    
    * Delete string-art module
    
    * Change exception call
    
    Co-authored-by: Ken Odegard <kodegard@anaconda.com>
    
    * Update ascii graph module to raise exceptions properly, change summary
    
    * Utilize argparse in subcommand module
    
    Co-authored-by: Ken Odegard <kodegard@anaconda.com>

---
+++ ascii_graph.py
@@ -0,0 +1,27 @@
+from sympy import symbols
+from sympy.plotting import textplot
+
+import argparse
+import conda.plugins
+
+
+def ascii_graph(argv: list):
+    parser = argparse.ArgumentParser("conda ascii-graph")
+
+    parser.add_argument("x", type=float, help="First coordinate to graph")
+    parser.add_argument("y", type=float, help="Second coordinate to graph")
+    parser.add_argument("z", type=float, help="Third coordinate to graph")
+
+    args = parser.parse_args(argv)
+
+    s = symbols('s')
+    textplot(s**args.x,args.y,args.z)
+
+
+@conda.plugins.register
+def conda_subcommands():
+    yield conda.plugins.CondaSubcommand(
+        name="ascii-graph",
+        summary="A subcommand that takes three coordinates and prints out an ascii graph",
+        action=ascii_graph,
+        )
