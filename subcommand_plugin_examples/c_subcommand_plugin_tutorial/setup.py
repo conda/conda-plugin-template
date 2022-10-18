3099b7728c2bd3482fc702afbdad54c9ad950ef4
Daniel Holth <dholth@anaconda.com>
2022-10-18 14:39:43 -0400

    move packages= to pyproject.toml

--- subcommand_plugin_examples/c_subcommand_plugin_tutorial/setup.py
+++ subcommand_plugin_examples/c_subcommand_plugin_tutorial/setup.py
@@ -1,8 +1,4 @@
 # setup.py (with automatic dependency tracking)
 from setuptools import setup
 
-setup(
-    packages=["temp_converter"],
-    cffi_modules=["temp_converter/builder.py:ffibuilder"],
-    install_requires=["cffi>=1.0.0"],
-)
+setup(cffi_modules=["temp_converter/builder.py:ffibuilder"])
