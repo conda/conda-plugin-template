717d4ccb64b3fc8289e06c2085ebfca787437a4c
Daniel Holth <dholth@anaconda.com>
2022-10-18 14:48:34 -0400

    update setup.py

--- subcommand_plugin_examples/c_subcommand_plugin_tutorial/setup.py
+++ subcommand_plugin_examples/c_subcommand_plugin_tutorial/setup.py
@@ -1,4 +1,4 @@
-# setup.py (with automatic dependency tracking)
+# setup.py (required for cffi_modules parameter)
 from setuptools import setup
 
 setup(cffi_modules=["temp_converter/builder.py:ffibuilder"])
