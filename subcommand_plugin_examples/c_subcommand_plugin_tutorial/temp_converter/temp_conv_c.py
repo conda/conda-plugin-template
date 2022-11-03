7da33cd3d218edbee3a5e82a0fd9818db3a63aa3
Daniel Holth <dholth@anaconda.com>
2022-11-03 10:27:27 -0400

    use cffi to compile shared library (#16)
    * use cffi to compile shared library
    * move packages= to pyproject.toml
    * update pyproject.toml for ascii_graph
    * update setup.py

--- subcommand_plugin_examples/c_subcommand_plugin_tutorial/temp_converter/temp_conv_c.py
+++ subcommand_plugin_examples/c_subcommand_plugin_tutorial/temp_converter/temp_conv_c.py
@@ -1,12 +1,10 @@
-from ctypes import *
 from conda.plugins import hooks
+
+from ._converter import lib
 
 
 def conda_temp_converter(*args, **kwargs):
-    so_file = "conda-plugin-template/subcommand_plugin_examples/c_subcommand_plugin_tutorial/temp_converter/c_to_f.so"
-    # The string above should be a relative path that points to the location of the c_to_f.so file!
-    my_functions = CDLL(so_file)
-    return my_functions.converter()
+    lib.converter()
 
 
 @hooks.register
