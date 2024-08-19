1cc705a4a8e74980a0c8a6c8b95d106064494074
Travis Hathaway <travis.j.hathaway@gmail.com>
2024-08-19 14:36:35 +0200

    adding a develop.sh script and .gitignore; updating a few other files

---
+++ cookiecutter-template/{{cookiecutter.project_slug}}/develop.sh
@@ -0,0 +1,15 @@
+# Used to switch CONDA_EXE to the one located in development environment
+# To use this script, run `source develop.sh`
+
+if ! (return 0 2> /dev/null); then
+    echo "ERROR: Source this script: source '$0'." >&2
+    exit 1
+fi
+
+CONDA_ENV_DIR="./env"
+
+conda env $(test -d "$CONDA_ENV_DIR" && echo update || echo create) -p "$CONDA_ENV_DIR" --file environment.yml
+conda activate "$CONDA_ENV_DIR"
+pip install -e .
+
+CONDA_EXE="$CONDA_PREFIX/condabin/conda"
